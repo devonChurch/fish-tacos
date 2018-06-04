35bc415eae6a184b2200298c869aaf41e7a63eee
strivegfx <notifydevon@gmail.com>
2018-06-04 20:07:03 +1200

    chore: clean up build
    clean up build with the removal of clean plugin and addition of peer dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const path = require("path");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const PRODUCTION_ENV = "production";
-const DEVELOPMENT_ENV = "development";
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const PRODUCTION_ENV = 'production';
+const DEVELOPMENT_ENV = 'development';
 const { NODE_ENV = PRODUCTION_ENV } = process.env;
 const isProduction = NODE_ENV === PRODUCTION_ENV;
 const dirDist = path.resolve(__dirname);
-const dirSrc = path.resolve(__dirname, "src");
-const libraryName = "fishTacos";
+const dirSrc = path.resolve(__dirname, 'src');
+const libraryName = 'fishTacos';
 
 const config = {
   mode: isProduction ? PRODUCTION_ENV : DEVELOPMENT_ENV,
@@ -15,31 +15,31 @@ const config = {
 
   output: {
     path: dirDist,
-    filename: "index.js",
+    filename: 'index.js',
     library: libraryName,
-    libraryTarget: "umd",
-    globalObject: "typeof self !== 'undefined' ? self : this"
+    libraryTarget: 'umd',
+    globalObject: "typeof self !== 'undefined' ? self : this",
   },
 
-  devtool: isProduction ? "source-map" : "cheap-source-map",
+  devtool: isProduction ? 'source-map' : 'cheap-source-map',
 
-  stats: isProduction ? "normal" : "errors-only",
+  stats: isProduction ? 'normal' : 'errors-only',
 
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
-        include: dirSrc
-      }
-    ]
+        use: 'ts-loader',
+        include: dirSrc,
+      },
+    ],
   },
 
   resolve: {
-    extensions: [".ts"]
+    extensions: ['.ts'],
   },
 
-  plugins: [new CleanWebpackPlugin(dirDist)]
+  // plugins: [new CleanWebpackPlugin(dirDist)]
 };
 
 module.exports = config;
